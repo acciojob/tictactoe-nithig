f5e9c56f5d8eac80b8235255222804c775818c1a
nithig <154483033+nithig@users.noreply.github.com>
2025-05-26 11:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ let players = [];
     ];
 
 submitBtn.addEventListener("click", function(){
-	const player1 = document.getElementById('player-1').value.trim();
-	const player2 = document.getElementById('player-2').value.trim();
+	const player1 = document.getElementById('player1').value.trim();
+	const player2 = document.getElementById('player2').value.trim();
 
 	if(!player1 || !player2){
 		alert("Please Fill all Fields!");
